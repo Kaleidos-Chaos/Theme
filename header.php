d19e2340ff67eb1f7ea27cd3e3be6beb58dc7bfd
Hannes Diercks <info@xiphe.net>
2013-08-26 15:20:06 +0200

    Add favicon

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/favicon.png">
         <!--[if lt IE 9]>
             <script src="<?php echo get_template_directory_uri(); ?>/javascripts/html5.js"></script>
         <![endif]-->
