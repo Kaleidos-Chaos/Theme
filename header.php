910d69538ddb06b661044a05aaf6d93dcc95655c
Hannes Diercks <info@xiphe.net>
2013-09-04 02:00:36 +0200

    blockquotes, title and spacings

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?php wp_title( '|', true, 'right' ); ?></title>
+        <title><?php wp_title( '|', true, 'right' ); bloginfo('name'); ?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
